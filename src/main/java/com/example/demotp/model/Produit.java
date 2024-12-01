06c12a0d1304bc8778e447f258ce62291a45340b
unknown <salimdagdou44@gmail.com>
2024-12-01 17:27:03 +0100

    add stock

--- src/main/java/com/example/demotp/model/Produit.java
+++ src/main/java/com/example/demotp/model/Produit.java
@@ -1,9 +1,16 @@
 package com.example.demotp.model;
 
+import java.util.ArrayList;
+
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +24,12 @@ public class Produit {
  private Long id;
  private String libelle;
  private Double prix;
- private Integer qteStock; 
+ private Integer qteStock=0; 
+ @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+ private Stock stock;
+ 
+
  // getters and setters
+
 
 }
